da8fd0b958e79cc13027d193cd63e0e3f93a2231
Kelvin Kamau <njengak993@gmail.com>
2026-05-13 06:17:36 +0300

    I added a few more pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,35 @@
+import MainLayout from "../layouts/MainLayout";
+
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        Hotel Website
-      </h1>
-    </div>
+    <MainLayout>
+
+      <section className="min-h-screen flex items-center justify-center">
+        <div className="container-custom text-center">
+
+          <h1 className="section-title">
+            Indigenous Seeds Village
+          </h1>
+
+          <p className="max-w-2xl mx-auto mt-6">
+            Eco-luxury hospitality inspired by indigenous heritage,
+            organic living, and nature-centered experiences.
+          </p>
+
+          <div className="flex gap-4 justify-center mt-8 flex-wrap">
+            <button className="primary-btn">
+              Book a Stay
+            </button>
+
+            <button className="secondary-btn">
+              Explore More
+            </button>
+          </div>
+
+        </div>
+      </section>
+
+    </MainLayout>
   );
 };
 
